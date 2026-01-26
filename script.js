9fc4f49d7b2e5539476759de86e5bdca1cc99103
Isaac Monteiro <isaacmonteiro.fm@gmail.com>
2026-01-26 18:28:21 -0300

    Creating script for add new list item in html

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function mainScript() {
+
   //Acessando formulário
   const form = document.querySelector("form")
 
   //Detectando evento de submit
   form.addEventListener("submit",(event) =>{
+
     //Impendindo que a página recarregue
     event.preventDefault()
 
@@ -11,17 +13,73 @@ function mainScript() {
     console.log("Evento submit detectado!")
 
     //Capturando valor recebido pelo input
-    newProduct = getProductInput()
+    const newProduct = getProductInput()
     console.log(`Capturando produto registrado pelo usuário: ${newProduct}`)
+
+    //Adicionando novo item na lista
+    addNewListItem(newProduct)
+
   })
 }
 
 function getProductInput() {
-  //Capturando valor
-  inputValue = document.querySelector("form input").value
+
+  //Acessando o <input>
+  const inputBox = document.querySelector("form input")
+
+  //Recebendo o valor registrado
+  const inputValue = inputBox.value
+
+  //Limpando campo do input
+  inputBox.value = ""
 
   //Retornando valor capturado
   return inputValue
+}
+
+function addNewListItem(itemValue) {
+
+  //Acessando informações da tag <ul>
+  const list = document.querySelector("ul")
+
+  //Criando novo elemento para lista
+  const newElement = createNewElement(itemValue)
+
+  //Adicionando newElement na lista
+  list.append(newElement)
+  
+}
+
+function createNewElement(itemValue) {
+
+  //Criando tags
+  const listItem = document.createElement("li")
+  const checkbox = document.createElement("input")
+  const span = document.createElement("span")
+  const button = document.createElement("button")
+  const img = document.createElement("img")
+
+  //Definindo tipo do <input>
+  checkbox.type = "checkbox"
+
+  //Definindo tipo do <button>
+  button.type = "button"
+
+  //Adicionando o produto registrado em <span>
+  span.textContent = itemValue
+
+  //Definindo caminho da imagem de lixeira
+  img.src = "assets/trash.svg"
+  img.alt = "ícone de lixeira"
+
+  //Adicionando <img> no <button>
+  button.append(img)
+
+  //Por fim, adicionando os elementos em <li>
+  listItem.append(checkbox, span, button)
+
+  //Retornando <li> criado
+  return listItem
 }
 
 mainScript()
